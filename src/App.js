dc9134ebe6617996f8f21b5a3708226751d1ddd3
Onowu Christopher <totiruzi@yahoo.com>
2020-06-09 04:38:34 +0300

    Integrate firebase into application, Create sign in page with sign in with google

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import Header from './components/header/header.component'
+import Header from './components/header/header.component';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import SigInAndSignUp from './components/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import SigInAndSignUp from './components/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import ShopPage from './pages/shop/shop.component';
+import { auth } from './firebase/firebase.utils';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentUser: null };
+  }
 
-function App() {
-  return (
-    <div>
-      <BrowserRouter>
-      <Header />
-        <Switch>
-          <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SigInAndSignUp} />
-          <Route exat path="/" component={HomePage} />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    });
+  }
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Header currentUser={this.state.currentUser}/>
+          <Switch>
+            <Route path="/shop" component={ShopPage} />
+            <Route path="/signin" component={SigInAndSignUp} />
+            <Route exat path="/" component={HomePage} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: 'AIzaSyCY6voLH6uTE-5pk-q45wqvrsxK_nN5ab4',
+  authDomain: 'gab-dan-cloths-db.firebaseapp.com',
+  databaseURL: 'https://gab-dan-cloths-db.firebaseio.com',
+  projectId: 'gab-dan-cloths-db',
+  storageBucket: 'gab-dan-cloths-db.appspot.com',
+  messagingSenderId: '764495895252',
+  appId: '1:764495895252:web:6b4877fa9b063491fef941',
+  measurementId: 'G-5P1G20ST5J',
+};
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
