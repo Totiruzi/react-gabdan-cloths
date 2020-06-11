5ea5b9fb630ada22b7310098b09fe91e7f976dc9
Onowu Christopher <totiruzi@yahoo.com>
2020-06-11 06:58:10 +0300

    Add redux, Implement userReducer, userActions to header and app component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,30 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Header from './components/header/header.component';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import SigInAndSignUp from './components/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import ShopPage from './pages/shop/shop.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = { currentUser: null };
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userReef = await createUserProfileDocument(userAuth);
         userReef.onSnapshot((snapShot) => {
-          this.setState(
-            {
-              currentUser: {
-                id: snapShot.id,
-                ...snapShot.data(),
-              },
-            }
-            // () => {
-            //   console.log(this.state);
-            // }
-          );
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data(),
+          });
         });
       } else {
-        this.setState({
-          currentUser: userAuth,
-        });
+        setCurrentUser({ userAuth });
       }
     });
   }
@@ -46,17 +35,18 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <BrowserRouter>
-          <Header currentUser={this.state.currentUser} />
-          <Switch>
-            <Route path="/shop" component={ShopPage} />
-            <Route path="/signin" component={SigInAndSignUp} />
-            <Route exat path="/" component={HomePage} />
-          </Switch>
-        </BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SigInAndSignUp} />
+          <Route exat path="/" component={HomePage} />
+        </Switch>
       </div>
     );
   }
 }
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
 
-export default App;
+export default connect(null, mapDispatchToProps)(App);
