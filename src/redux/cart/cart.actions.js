c06d3e25c5c86458965bf2c57f47afc505ab0cdf
Onowu Christopher <totiruzi@yahoo.com>
2020-06-12 03:13:13 +0300

    Udate shop data with new data,Udate cart utils to group items in cart reducer

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,6 +1,8 @@
 import CartActionTypes from './cart.types';
-import CartActionTpes from './cart.types';
-
 export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN
+})
+export const addItem = item => ( {
+  type: CartActionTypes.ADD_ITEM,
+  payload: item
 })
--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,4 +1,5 @@
 const CartActionTypes = {
   TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
+  ADD_ITEM: 'CART_ITEM'
 };
 export default CartActionTypes;
